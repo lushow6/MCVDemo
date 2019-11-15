ceaaa6d71b84513a6519bb737781a255006c6745
Difei Lu <ldfghh@uw.edu>
2019-11-14 18:15:07 -0800

    Toturial completed

---
+++ MCVDemo/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MCVDemo.Models;
+
+namespace MCVDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+
+            List<PersonModel> people = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "SB", LastName = "Lu", Age = 25 },
+                new PersonModel { FirstName = "Coco", LastName = "Li", Age = 23 },
+                new PersonModel { FirstName = "Zozo", LastName = "Liu", Age = 28 }
+            };
+
+            return View(people);
+        }
+    }
+}
--- MCVDemo/Models/PersonModel.cs
+++ MCVDemo/Models/PersonModel.cs
@@ -7,5 +7,10 @@ namespace MCVDemo.Models
 {
     public class PersonModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+
     }
 }
